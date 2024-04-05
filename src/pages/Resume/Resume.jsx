fa18895b799bea019f02ee6397cd5b50abd1a8f7
JulenHopp <julenhoppenstedt@gmail.com>
2024-04-05 13:54:04 -0600

    Progress in resume section

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import resumePDF from '/src/data/resume_JulenHoppenstedtMandiola.pdf';
+
 
 export default function Resume() {
   return (
     <>
-      <p>Resume</p>
+      <div>
+        <div>
+          <iframe src="/src/data/resume_JulenHoppenstedtMandiola.pdf" width="100%" height="100%" title="Resume"/>
+        </div>
+      </div>
     </>
   );
 }
