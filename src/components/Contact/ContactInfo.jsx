d9820b1c083e3b04ca9c210284650d11cc58d49c
JulenHopp <julenhoppenstedt@gmail.com>
2024-05-24 16:37:25 -0600

    Finalizing v3 1.0

--- src/components/Contact/ContactInfo.jsx
+++ src/components/Contact/ContactInfo.jsx
@@ -8,9 +8,9 @@ export default function ContactInfo() {
                 <h3 className="contact-title">Contact Information</h3>
                 <p className='contact-subTitle'>Feel free to reach out for collaborations or inquiries!</p>
                 <p className="contact-detail"><b>Phone:</b><br /> +52 (961) 117 3308</p>
-                <p className="contact-detail"><b>Email:</b><br /> jhm_e@outlook.com</p>
-                <p className="contact-detail"><b>LinkedIn:</b><br /> linkedin.com/in/julenhopp</p>
-                <p className="contact-detail"><b>GitHub:</b><br /> github.com/JulenHopp</p>
+                <a href="mailto:jhm_e@outlook.com" target="_blank" rel="noopener noreferrer"><p className="contact-detail"><b>Email:</b><br /> jhm_e@outlook.com</p></a>
+                <a href="https://www.linkedin.com/in/julenhopp" target="_blank" rel="noopener noreferrer"><p className="contact-detail"><b>LinkedIn:</b><br /> linkedin.com/in/julenhopp</p></a>
+                <a href="https://github.com/JulenHopp" target="_blank" rel="noopener noreferrer"><p className="contact-detail"><b>GitHub:</b><br /> github.com/JulenHopp</p></a>
             </div>
         </div>
     );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import  React from 'react';
+import './Home.css';
 import HomeData from '/src/data/homeComponentData';
 import HomeComponent from '/src/components/Home/HomeComponent';
 
 export default function Home() {
   return (
-    <>
+    <div className='home-container'>
       <HomeComponent props={HomeData}/>
-    </>
+    </div>
   );
 }
